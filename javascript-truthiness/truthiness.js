653e60123cf997b80424ce21e4114af7fc9d2a7a
Tom Aratyn <tom@aratyn.name>
2013-09-04 17:23:12 -0400

    added truthiness demo.

---
+++ javascript-truthiness/truthiness.js
@@ -0,0 +1,114 @@
+/*
+ * Copyright 2013 Tom Aratyn <tom@aratyn.name>
+ */
+
+/* You can execute this file in Firefox Scratchpad by:
+ * 1. Open Firefox
+ * 2. Open ScratchPad (Tools-> Web Developer -> Scratchpad or Shift + F4)
+ * 3. Open this file (File-> Open then find the file)
+ * 4. Run the file (Execute-> Run)
+ * 
+ * In JavaScript something is truthy when it evaluates to true. 
+ *
+ * BUT not all things which evaluate to true neccessairily EQUAL (==) to true.
+ * (Obviously nothing except true is Strictly Equal (===) to true)
+ * Let's look at some demos:
+ */
+
+var anArray = [],
+    anObject = {},
+    aString = "", 
+    aNumber = 0;
+
+console.log("anArray, which is empty, is NOT equal to true: " + (anArray == true));
+
+if (anArray) {
+  console.log("but anArray evaluated to true in the condition of an if statement");
+}
+else {
+  console.log("if you see this message, the demo has been modified.");
+}
+
+console.log("anObject, which is also empty, is also not equal to true: " + (anObject == true));
+
+if (anObject) {
+  console.log("but anObject evaluated to true in the condition of an if statement");
+}
+else {
+  console.log("if you see this message, the demo has been modified.");
+}
+
+console.log("aString, which is also empty, is also not equal to true: " + (aString == true));
+
+if (aString) {
+  console.log("if you see this message, the demo has been modified.");
+}
+else {
+  console.log("but aString, which is empty, evaluated to false in the condition of an if statement");
+}
+
+console.log("aNumber, zero, is also not equal to true: " + (aNumber == true));
+
+if (aNumber) {
+  console.log("if you see this message, the demo has been modified.");
+}
+else {
+  console.log("but aNumber, which is zero, evaluated to false in the condition of an if statement");
+}
+
+/* Let's see how it's different if these objects have values */
+
+anArray = [1, 2, 3];
+
+console.log("anArray, which contains numbers, is not equal to true: " + (anArray == true));
+
+if (anArray) {
+  console.log("but anArray evaluated to true in the condition of an if statement");
+}
+else {
+  console.log("if you see this message, the demo has been modified.");
+}
+
+anObject = {a: 1};
+
+console.log("anObject, which is not empty, is also not equal to true: " + (anObject == true));
+
+if (anObject) {
+  console.log("but anObject evaluated to true in the condition of an if statement");
+}
+else {
+  console.log("if you see this message, the demo has been modified.");
+}
+
+aString = "And on earth let there be peace to all people of good will.";
+
+console.log("aString, which is not empty, is also not equal to true: " + (aString == true));
+
+if (aString) {
+  console.log("but aString, which is not empty, evaluated to false in the condition of an if statement");
+}
+else {
+  console.log("if you see this message, the demo has been modified.");
+
+}
+
+aNumber = 5;
+
+console.log("aNumber, five, is also not equal to true: " + (aNumber == true));
+
+if (aNumber) {
+  console.log("but aNumber, which is not zero, evaluated to true in the condition of an if statement");
+}
+else {
+  console.log("if you see this message, the demo has been modified.");
+}
+
+
+/* What conclusion do we draw?
+ * 
+ * "falsey" and "truthy" are not purely about being equal (==)  to true or false. 
+ *
+ * Most things will be truthy except:
+ * 
+ * undefined, null, NaN, 0, "" (empty string), and false. 
+ */ 
