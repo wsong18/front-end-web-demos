22ec81aa5ac789766ae5940276dad50b969ec080
Tom Aratyn <tom@aratyn.name>
2013-09-23 09:43:48 -0400

    simplified the regexp demos.

--- javascript-regexp/RegExpLiteral.js
+++ javascript-regexp/RegExpLiteral.js
@@ -1,10 +1,8 @@
 /* Copyright Tom Aratyn <tom@aratyn.name> */
 
-var hexNumberMatch = /^[a-f0-9]{6}|[a-f0-9]{3}$/,
-  red = "f00",
+var hexNumberMatch = /^[a-f0-9]{6}$/,
   white = "ffffff",
   invalidColor = "ff0gaa";
 
-console.log('does red match as a hex number? ' + hexNumberMatch.test(red));
 console.log('does white match as a hex number? ' + hexNumberMatch.test(white));
 console.log('does invalidColor match as a hex number? ' + hexNumberMatch.test(invalidColor));
